63ea82e71bf71bc4d6a8ed1252b689d2186e6de7
Davor Skulj <davor.skulj@gmail.com>
2026-05-28 15:46:14 +0200

    Split drawing component (#30)

--- packages/runtime/components/index.d.ts
+++ packages/runtime/components/index.d.ts
@@ -1,22 +1,12 @@
 /**
  * Forge Components aggregate entrypoint.
  */
-import { ForgeUIElement } from './base.js';
 export * from './actions.js';
 export * from './content.js';
 export * from './data.js';
+export * from './drawing.js';
 export * from './feedback.js';
 export * from './input.js';
 export * from './layout.js';
 export * from './navigation.js';
-export declare class ForgeDrawing extends ForgeUIElement {
-    static get properties(): {
-        props: {
-            type: ObjectConstructor;
-        };
-    };
-    static get styles(): import("lit").CSSResult;
-    render(): import("lit-html").TemplateResult<2>;
-    renderShape(s: any): import("lit-html").TemplateResult<2>;
-}
 //# sourceMappingURL=index.d.ts.map
